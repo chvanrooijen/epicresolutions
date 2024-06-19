7263a42f848878f5644fedfb886a4f2ebc28addc
chvanrooijen <rick.vanrooijen@vr-oa.nl>
2024-06-19 18:19:34 +0200

    Added features to create personal list

--- resolutions/urls.py
+++ resolutions/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'resolutions'
+
 urlpatterns = [
     path('select_role/', views.select_role, name='select_role'),
     path('select_cause/', views.select_cause, name='select_cause'),
-    path('generate_resolutions/', views.generate_resolutions, name='generate_resolutions'),
+    path('select_resolutions/', views.select_resolutions, name='select_resolutions'),
+    path('personal_list/', views.personal_list, name='personal_list'),
 ]
--- resolutions/views.py
+++ resolutions/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect # Redirect is a new import to save the Roles and Causes in the session and then use these values to filter the Resolutions.
+from django.shortcuts import render, redirect, get_object_or_404 # Redirect is a new import to save the Roles and Causes in the session and then use these values to filter the Resolutions.
 from .forms import RoleForm, CauseForm
 from .models import Role, Cause, Resolution
+from django.http import JsonResponse
+from uuid import uuid4
 
 # Create your views here.
 def select_role(request):
@@ -10,7 +12,7 @@ def select_role(request):
             roles = form.cleaned_data['roles']  # Get the Role instances
             role_names = [role.name for role in roles]  # Get the names from the Role instances
             request.session['role_names'] = role_names  # Save the Role names in the session.
-            return redirect('select_cause')
+            return redirect('resolutions:select_cause')
     else:
         form = RoleForm()
     return render(request, 'resolutions/select_role.html', {'form': form})
@@ -22,12 +24,12 @@ def select_cause(request):
             causes = form.cleaned_data['causes']  # Get the Cause instances
             cause_names = [cause.name for cause in causes]  # Get the names from the Cause instances
             request.session['cause_names'] = cause_names  # Save the Cause names in the session.
-            return redirect('generate_resolutions')
+            return redirect('resolutions:select_resolutions')
     else:
         form = CauseForm()
     return render(request, 'resolutions/select_cause.html', {'form': form})
 
-def generate_resolutions(request):
+def select_resolutions(request):
     role_names = request.session.get('role_names', [])  # Get the Role names from the session
     cause_names = request.session.get('cause_names', [])  # Get the Cause names from the session
     # Get the Role and Cause instances based on the names
@@ -48,4 +50,18 @@ def generate_resolutions(request):
         'causes': causes,
     }
 
-    return render(request, 'resolutions/resolutions.html', context)
+    return render(request, 'resolutions/select_resolutions.html', context)
+
+def personal_list(request):
+    if request.method == 'POST':
+        included_roles = request.POST.getlist('include_role')
+        included_resolutions = request.POST.getlist('include_resolution')
+
+        roles = Role.objects.filter(id__in=included_roles)
+        resolutions = Resolution.objects.filter(id__in=included_resolutions)
+
+        return render(request, 'resolutions/personal_list.html', {
+            'roles': roles,
+            'resolutions': resolutions,
+        })
+    
