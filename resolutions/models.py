8a5d6b7d381ab2073c7a88672e954fb6718fe1ac
chvanrooijen <rick.vanrooijen@vr-oa.nl>
2024-09-11 14:51:54 +0200

    Update models.py to limit Role name length to 100 characters

--- resolutions/models.py
+++ resolutions/models.py
@@ -3,10 +3,13 @@ from django.db import models
 
 # Create your models here.
 class Role(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name'] # order by name
 
 
 class Cause(models.Model):
