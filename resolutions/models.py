0a5fdd8de5eb03b3212793d228cf45456b6679f0
chvanrooijen <rick.vanrooijen@vr-oa.nl>
2024-10-23 16:30:06 +0200

    Refactor CSS height in style.css and add details to resolution display in select-resolutions.html

--- resolutions/models.py
+++ resolutions/models.py
@@ -30,3 +30,18 @@ class Resolution(models.Model):
 
     def __str__(self):
         return self.positive_action
+    
+    def get_article(self, word):
+        return 'an' if word[0].lower() in 'aeiou' else 'a'
+
+    def capitalize_sentence(self, sentence):
+        return sentence[0].upper() + sentence[1:]
+
+    def to_sentence(self):
+        causes = ', '.join(cause.name.lower() for cause in self.causes.all())
+        article = self.get_article(self.role.name.lower())
+        sentence = (f"As {article} {self.role.name.lower()}, I {self.positive_action.lower()}, "
+                    f"when {self.trigger.lower()}, to {self.goal.lower()}, and {self.incentive.lower()}, "
+                    f"rather than {self.negative_action.lower()}. "
+                    f"Causes: {causes}.")
+        return self.capitalize_sentence(sentence)
